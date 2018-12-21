570af61f23b3185acef78d23d7913a667236b8e2
ms747 <mayur.shah.ha@gmail.com>
2018-12-21 15:05:55 +0530

    ip list

---
+++ src/iplist.js
@@ -0,0 +1,101 @@
+const ip = [
+  {
+    ip: "10.10.10.12",
+    name: "srv1",
+    childs: [
+      {
+        ip: "10.10.10.121",
+        name: "erpdbsrv",
+        id: 103
+      },
+      {
+        ip: "10.10.10.105",
+        name: "pshptapp",
+        id: 107
+      }
+    ]
+  },
+  {
+    ip: "10.10.10.13",
+    name: "srv2",
+    childs: [
+      {
+        ip: "10.10.10.104",
+        name: "psql",
+        id: 109
+      },
+      {
+        ip: "10.10.10.103",
+        name: "pssrs",
+        id: 111
+      }
+    ]
+  },
+  {
+    ip: "10.10.10.14",
+    name: "srv3",
+    childs: [
+      {
+        ip: "10.10.10.107",
+        name: "pemailrt",
+        id: 106
+      },
+      {
+        ip: "10.10.10.109",
+        name: "psync",
+        id: 110
+      },
+      {
+        ip: "10.10.10.108",
+        name: "newbiometeric",
+        id: 125
+      }
+    ]
+  },
+  {
+    ip: "10.10.10.15",
+    name: "srv4",
+    childs: [
+      {
+        ip: "10.10.10.101",
+        name: "prodcrm",
+        id: 101
+      },
+      {
+        ip: "10.10.10.120",
+        name: "erpappsrv",
+        id: 103
+      },
+      {
+        ip: "10.10.10.106",
+        name: "pshptdb",
+        id: 108
+      },
+      {
+        ip:"10.10.10.110",
+        name:"uappemlrt",
+        id:112
+    },
+    {
+        ip:"10.10.10.111",
+        name:"ushptapp",
+        id:113
+    },
+    {
+        ip:"10.10.10.114",
+        name:"dcrmappeml",
+        id:114
+    },
+    {
+        ip:"10.10.10.130",
+        name:"bitdefender",
+        id:120
+    },
+    {
+        ip:"10.10.10.131",
+        name:"bitdefender2",
+        id:121
+    },
+    ]
+  }
+];
